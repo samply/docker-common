9443b9c83a85e5d976728c9dee8f554eb5187f0c
Torben Brenner <torben.brenner+accounts@googlemail.com>
2020-04-18 00:17:43 +0200

    Added optional third parameter for bash script

--- tomcat/build.sh
+++ tomcat/build.sh
@@ -5,11 +5,12 @@ if [ -z "$1"] || [ -z "$2" ]; then
 fi
 COMPONENT=$1; # The component for which an container should be build
 COMPONENT_REPOSITORY=$2; # The git clone link of components repository 
+COMMON_REPOSITORY_COMMIT=${3:-970e0b61ca72421c53db40279b26dc140dc173b8};
 docker build -t $COMPONENT-builder --build-arg COMPONENT=$COMPONENT -f- $COMPONENT_REPOSITORY < Dockerfile.builder;
 if [ -d "$(pwd)/artifacts" ]; then
   rm -r artifacts;
 fi
 docker run -it --rm --name $COMPONENT-build -v /$(pwd)/artifacts://usr/src/$COMPONENT-build/target -v maven-home:/root/.m2 $COMPONENT-builder mvn install;
 ### This should not take context. It should take WAR File produced in step before
-docker build -t $COMPONENT:wip --build-arg COMMIT_HASH=5850e29bcbfb6f375a9151af87d6e06ba2b9a146 -f- $(pwd)/artifacts/ < Dockerfile;
+docker build -t $COMPONENT:wip --build-arg COMMIT_HASH=$COMMON_REPOSITORY_COMMIT -f- $(pwd)/artifacts/ < Dockerfile;
 exit 0;
