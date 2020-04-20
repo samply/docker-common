02190ef6b35206d466a00ebb7a9fdfc47210bd58
Torben Brenner <torben.brenner+accounts@googlemail.com>
2020-04-20 16:44:06 +0200

    Switched to named options
    The reason for this is that not all options need to be defined

--- README.md
+++ README.md
@@ -32,3 +32,11 @@ Pull requests are welcome. TODO:
 Please make sure to update tests as appropriate.
 
 TODO: License
+
+### Known Issues
+# Dockerignore in Repositories will be used
+Passing the repository as build context can lead to issues with building the specific repository.
+For example using https://code.mitro.dkfz.de/scm/auth/samply.auth.webapp.git#develop in the -r
+option will lead to failing of the maven build with message maven goal not defined. The reason for
+this is, that the repository will be copied to build context, but docker checks .dockerignore in the
+context before copying everything. Pom is excluded by .dockerignore in this specific case.
--- tomcat/build.sh
+++ tomcat/build.sh
@@ -1,17 +1,31 @@
 #!/bin/bash -e
-if [ -z "$1"] || [ -z "$2" ]; then
-  echo "Please specify COMPONENT (first parameter) and COMPONENT_REPOSITORY(second parameter)";
-  exit 1;
-fi
-COMPONENT=$1; # The component for which an container should be build
-COMPONENT_REPOSITORY=$2; # The git clone link of components repository 
-COMMON_REPOSITORY_COMMIT=${3:-970e0b61ca72421c53db40279b26dc140dc173b8};
-MAVEN_HOME=${4:-maven-home}
+# COMPONENT=$1; # The component for which an container should be build
+# COMPONENT_REPOSITORY=$2; # The git clone link of components repository
+COMMON_REPOSITORY_COMMIT="1a76e2dd03f1816cf4c7007d991e14d9eb700e8b";
+MAVEN_HOME="maven-home";
+# if [ -z "$COMPONENT" ] || [ -z "$COMPONENT_REPOSITORY" ]; then
+#   echo "The arguments COMPONENT and COMPONENT_REPOSITORY are mandatory. Please specify them.";
+#   exit 1;
+# fi
+while getopts "c:r:h:m:" OPTION
+do
+  case "${OPTION}"
+    in
+    c)
+      COMPONENT=${OPTARG};;
+    r)
+      COMPONENT_REPOSITORY=${OPTARG};;
+    h)
+      COMMON_REPOSITORY_COMMIT=${OPTARG};;
+    m)
+      MAVEN_HOME=${OPTARG};;
+  esac
+done
 docker build -t $COMPONENT-builder --build-arg COMPONENT=$COMPONENT -f- $COMPONENT_REPOSITORY < Dockerfile.builder;
 if [ -d "$(pwd)/artifacts" ]; then
   rm -r artifacts;
 fi
-docker run -it --rm --name $COMPONENT-build -v /$(pwd)/artifacts://usr/src/$COMPONENT-build/target -v maven-home://root/.m2 $COMPONENT-builder mvn install;
+docker run -it --rm --name $COMPONENT-build -v /$(pwd)/artifacts://usr/src/$COMPONENT-build/target -v $MAVEN_HOME://root/.m2 $COMPONENT-builder mvn install;
 ### This should not take context. It should take WAR File produced in step before
 docker build -t $COMPONENT:wip --build-arg COMPONENT=$COMPONENT --build-arg COMMIT_HASH=$COMMON_REPOSITORY_COMMIT -f- $(pwd)/artifacts/ < Dockerfile;
 exit 0;
