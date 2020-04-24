faa311753a792e4538907f8ee35aa480f3de9a68
Torben Brenner <torben.brenner+accounts@googlemail.com>
2020-04-24 09:30:00 +0200

    Added remote debug possibility to tomcat container

--- tomcat/scripts/tomcat_entrypoint.sh
+++ tomcat/scripts/tomcat_entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/bash -e
 source /scripts/common_entrypoint.sh
-### Start tomcat
-echo "Info: starting $COMPONENT tomcat ...";
-catalina.sh run
+### Configure tomcat
+
+if [ "$DEBUG" = 'true' ]; then
+	export JPDA_ADDRESS=1099;
+	export JPDA_TRANSPORT=dt_socket;
+	echo "Info: starting $COMPONENT tomcat with debug mode. Debug port is set to $JPDA_ADDRESS and JPDA_TRANSPORT is set to $JPDA_TRANSPORT";
+	exec catalina.sh jpda run;
+else
+  echo "Info: starting $COMPONENT tomcat ...";
+	exec catalina.sh run;
+fi
