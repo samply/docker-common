f6014acf6ff816ea62d591176aab8a7f11f5570c
Torben Brenner <t.brenner@dkfz-heidelberg.de>
2021-03-17 10:28:26 +0100

    fix: Switch to non-root user after entrypoint
    Importing certificates from custom-certs was not possible because of permission issues

--- tomcat/scripts/tomcat_entrypoint.sh
+++ tomcat/scripts/tomcat_entrypoint.sh
@@ -61,9 +61,9 @@ if [ "$DEBUG" = 'true' ]; then
 	export JPDA_ADDRESS=1099;
 	export JPDA_TRANSPORT=dt_socket;
 	echo "Info: starting $COMPONENT tomcat with debug mode. Debug port is set to $JPDA_ADDRESS and JPDA_TRANSPORT is set to $JPDA_TRANSPORT";
-	exec catalina.sh jpda run;
+	su -c 'exec catalina.sh jpda run;' $COMPONENT
 else
   ## Starting tomcat in productive mode
   echo "Info: starting $COMPONENT tomcat ...";
-	exec catalina.sh run;
+	su -c 'exec catalina.sh run;' $COMPONENT
 fi
